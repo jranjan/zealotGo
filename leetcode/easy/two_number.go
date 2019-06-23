b481e511787c03f938f595dbc0af4288da14bfcb
Jyoti <jranjan@gmail.com>
2019-06-23 11:08:17 +0530

    Update two_number.go

--- leetcode/easy/two_number.go
+++ leetcode/easy/two_number.go
@@ -1,8 +1,8 @@
 // Given an array of integers, return indices of 
 // the two numbers such that they add up to a specific target. 
 // You may assume that each input would have 
-// exactly one solution, and you may not use the same element twice.
-
+// exactly one solution, and you may not use the same 
+// element twice.
 
 func twoSum(nums []int, target int) []int {
     sum := 0
